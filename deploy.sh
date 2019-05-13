e7aa69c955f4750d37d72f620a0f49e442930e13
張仲豪 <goodjason@zhangzhonghaode-MacBook-Pro.local>
2019-05-14 02:22:45 +0800

    test13

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ git init
 git add -A
 git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
-git push -f https://99582ba52fef23f42579d392fb874c2be13ead69@github.com/EasonChang0115/EasonChang0115.github.io.git master
+git push -f https://${access_token}@github.com/EasonChang0115/EasonChang0115.github.io.git master
 # git push -f https://github.com/EasonChang0115/EasonChang0115.github.io.git master
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages/
