fa2e81112ecdf58aeda1e5aa6fa36f47cd928c57
EasonChang <s8710606@gmail.com>
2021-01-07 15:17:32 +0800

    [Refactor] 測試最後編輯格式

--- .vuepress/theme/components/Page.vue
+++ .vuepress/theme/components/Page.vue
@@ -118,6 +118,7 @@ export default {
       return false
     },
     lastUpdated () {
+      console.log(this.$page);
       return new Date(this.$page.lastUpdated).toLocaleString()
     },
     lastUpdatedText () {
