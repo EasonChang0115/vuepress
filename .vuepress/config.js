5d66ae6bb0d21443048a86a08d81935794cff6ae
EasonChang <s8710606@gmail.com>
2023-01-13 20:18:17 +0800

    feat: update pwa

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -19,6 +19,14 @@ module.exports = {
         content: 'width=device-width,initial-scale=1,user-scalable=no',
       },
     ],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
     [
       'meta',
       {
@@ -105,6 +113,13 @@ module.exports = {
     },
   },
   plugins: [
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: true
+      }
+    ],
     [
       '@vuepress/google-analytics',
       {
